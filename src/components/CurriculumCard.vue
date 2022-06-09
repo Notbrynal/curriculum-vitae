38d7bf529a0ab19e37838cc436733d27615c7236
Juan Reynosa <al157706@alumnos.uacj.mx>
2022-06-08 21:08:01 -0600

    Curriculum Card and Container

---
+++ src/components/CurriculumCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-col cols="12">
+    <v-card outlined style="border-radius: 15px;">
+      <v-row class="mt-1 mb-0">
+        <v-col class="mx-6 my-0 py-0"> 
+          <span class="" style="color: #FF5722;">
+            {{ title }}
+          </span> 
+          </v-col>
+      </v-row>
+      <v-row v-if="subHeader" class="my-0">
+        <v-col class="mx-6 my-0 py-0">
+          <span class="text-subtitle-2">
+            {{ subHeader }}
+          </span>
+        </v-col>
+      </v-row>
+      <v-row class="my-0">
+        <v-col class="mx-6 my-0 py-0">
+          <span class="text-caption font-weight-light">
+            {{ message }}
+          </span>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-col>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class CurriculumCard extends Vue {
+  @Prop() private title!: string;
+  @Prop() private subHeader!: string;
+  @Prop() private message!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <center>
+      <v-row class="mt-5" style="max-width: 700px; background-color: #ECEFF1;">
+        <v-col cols="12" sm="4" md="4">
+          <CurriculumCard
+            title="Juan De Dios Reynosa Silva"
+            message="Software Developer"
+          />
+          <CurriculumCard title="How to reach me" message="" />
+          <CurriculumCard
+            title="Personal interests"
+            message="Artificial Intelligence.
+              Data science.
+              Machine learning.
+              Internet Of Things.
+              Mobile software."
+          />
+        </v-col>
+        <v-col cols="12" sm="4" md="4">
+          <CurriculumCard
+            title="Personal Profile"
+            message="I am a Computer Science 
+              Engineer with robust problem solving skills and proven experience in creating 
+              and designing software.
+              "
+          />
+          <CurriculumCard
+            title="Education"
+            subHeader="Universidad Autónoma de Ciudad Juárez"
+            message="Bachelor's degree in Computer Science (2016-2021)."
+          />
+          <CurriculumCard
+            title="Professional summary"
+            subHeader="Network technician in UACJ 
+              (2019)."
+            message="Maintenance and installation of structured network 
+              and basic network management of switches and routers."
+          />
+        </v-col>
+        <v-col cols="12" sm="4" md="4">
+          <CurriculumCard title="Skills" message="message" />
+          <CurriculumCard title="Lenguages" message="message" />
+        </v-col>
+      </v-row>
+    </center>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import CurriculumCard from "@/components/CurriculumCard.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    CurriculumCard,
   },
 })
 export default class Home extends Vue {}
