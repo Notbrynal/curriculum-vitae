d3bedf2d967d10aad2a80c3be5d9b84f38fa8d00
Juan Reynosa <al157706@alumnos.uacj.mx>
2022-06-12 17:32:53 -0600

    adding dist

---
+++ precache-manifest.0845389fec86ba3612d157a29f8dea21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7356d58de0c3bb04e0cb",
+    "url": "/css/chunk-vendors.d19e80b0.css"
+  },
+  {
+    "revision": "cd8f6c33114c6e0e9ba9e0481670eb74",
+    "url": "/img/PersonalPicture.cd8f6c33.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "724dfafa3290968f64864756c0d70893",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e38e99a35bdeab316170",
+    "url": "/js/app.d8cef9fc.js"
+  },
+  {
+    "revision": "7356d58de0c3bb04e0cb",
+    "url": "/js/chunk-vendors.7a620836.js"
+  },
+  {
+    "revision": "e24a6ebd14c4fc6b6b9121a431d219ab",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
