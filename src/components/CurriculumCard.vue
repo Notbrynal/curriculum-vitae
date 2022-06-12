38f553eecd2760190dc68d5bc08a7ad788a3a432
Juan Reynosa <al157706@alumnos.uacj.mx>
2022-06-12 17:26:51 -0600

    First functional version

--- src/components/CurriculumCard.vue
+++ src/components/CurriculumCard.vue
@@ -1,27 +1,61 @@
 <template>
-  <v-col cols="12">
-    <v-card outlined style="border-radius: 15px;">
+  <v-col cols="12" :style="{ transform: image? 'translateY(-25px)' : '' }">
+    <v-avatar
+      size="150"
+      style="transform: translateY(25px); z-index: 5"
+      v-if="image"
+    >
+      <v-img src="@/assets/cardPicture/PersonalPicture.png"/>
+    </v-avatar>
+    <v-card outlined style="border-radius: 15px" :color="color">
       <v-row class="mt-1 mb-0">
-        <v-col class="mx-6 my-0 py-0"> 
-          <span class="" style="color: #FF5722;">
-            {{ title }}
-          </span> 
+        <v-col
+          :class="{
+            'mx-6 ': true,
+            'my-0': !image,
+            'py-0': true,
+            'mt-6': image,
+          }"
+          :style="{ marginTop: image ? '50px !important;' : '' }"
+        >
+          <!-- class="mx-6 my-0 py-0" -->
+          <span class="font-weight-bold" :style="{ color: title.color }">
+            {{ title.message }}
+          </span>
+        </v-col>
+      </v-row>
+      <div v-for="(element, index) in message" :key="index">
+        <v-row v-if="subHeader" :class="{ 'mt-2': subHeader[index] }">
+          <v-col class="mx-6 my-0 py-0">
+            <span class="text-caption font-weight-bold">
+              {{ subHeader[index] }}
+            </span>
           </v-col>
-      </v-row>
-      <v-row v-if="subHeader" class="my-0">
-        <v-col class="mx-6 my-0 py-0">
-          <span class="text-subtitle-2">
-            {{ subHeader }}
-          </span>
-        </v-col>
-      </v-row>
-      <v-row class="my-0">
-        <v-col class="mx-6 my-0 py-0">
-          <span class="text-caption font-weight-light">
-            {{ message }}
-          </span>
-        </v-col>
-      </v-row>
+        </v-row>
+        <v-row class="mt-0">
+          <v-col class="mx-6 my-0">
+            <span class="text-caption font-weight-light"  :style="{color: subHeader ? '#757575': 'white'}">
+              {{ message[index] }}
+            </span>
+          </v-col>
+        </v-row>
+      </div>
+      <div v-for="(bullet, index) in bullets" :key="index" class="mb-2">
+        <v-row class="mt-2">
+          <v-col class="mx-6 my-0 py-0">
+            <span class="text-caption font-weight-bold">
+              {{ bullet.title }}
+            </span>
+          </v-col>
+        </v-row>
+        <v-row class="mx-0 py-0" v-for="(element, index) in bullet.message " :key="index">
+          <v-col class="mx-6 my-0 py-0">
+            <span class="text-caption font-weight-light">
+              &#8226; {{ element }}
+            </span>
+          </v-col>
+        </v-row>
+      </div>
     </v-card>
   </v-col>
 </template>
@@ -31,9 +65,12 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
 export default class CurriculumCard extends Vue {
-  @Prop() private title!: string;
-  @Prop() private subHeader!: string;
-  @Prop() private message!: string;
+  @Prop() private color!: string;
+  @Prop() private image!: string;
+  @Prop() private title!: Record<string, unknown>;
+  @Prop() private subHeader!: [];
+  @Prop() private message!: [];
+  @Prop() private bullets!: Record<string, unknown>;
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,120 @@
 <template>
   <div>
     <center>
-      <v-row class="mt-5" style="max-width: 700px; background-color: #ECEFF1;">
-        <v-col cols="12" sm="4" md="4">
+      <v-row class="mt-0" style="max-width: 760px; background-color: #eceff1">
+        <v-col cols="12" sm="4" md="4" class="mt-2">
           <CurriculumCard
-            title="Juan De Dios Reynosa Silva"
-            message="Software Developer"
-          />
-          <CurriculumCard title="How to reach me" message="" />
-          <CurriculumCard
-            title="Personal interests"
-            message="Artificial Intelligence.
-              Data science.
-              Machine learning.
-              Internet Of Things.
-              Mobile software."
-          />
-        </v-col>
-        <v-col cols="12" sm="4" md="4">
-          <CurriculumCard
-            title="Personal Profile"
-            message="I am a Computer Science 
-              Engineer with robust problem solving skills and proven experience in creating 
-              and designing software.
-              "
+            image="@/assets/cardPicture/PersonalPicture.png"
+            color="#FF5722"
+            :title="{
+              message: 'Juan De Dios Reynosa Silva',
+              color: 'white',
+            }"
+            :message="['Software Developer']"
           />
           <CurriculumCard
-            title="Education"
-            subHeader="Universidad Autónoma de Ciudad Juárez"
-            message="Bachelor's degree in Computer Science (2016-2021)."
+            :title="{
+              message: 'How to reach me',
+              color: '#FF5722',
+            }"
+            :subHeader="['Cellphone', 'E-mail', 'LinkedIn', 'GitHub']"
+            :message="['656-789-2980', 'jreynosa.developer@gmail.com', 'https://www.linkedin.com/in/juan-de-dios-reynosa-silva-429018202/', 'https://github.com/Notbrynal']"
           />
           <CurriculumCard
-            title="Professional summary"
-            subHeader="Network technician in UACJ 
-              (2019)."
-            message="Maintenance and installation of structured network 
-              and basic network management of switches and routers."
+            :title="{
+              message: 'Personal interests',
+              color: '#FF5722',
+            }"
+            :subHeader="['Computer Science']"
+            :message="[
+              'Artificial Intelligence. Data science. Machine learning. Internet Of Things. Mobile software.',
+            ]"
           />
         </v-col>
-        <v-col cols="12" sm="4" md="4">
-          <CurriculumCard title="Skills" message="message" />
-          <CurriculumCard title="Lenguages" message="message" />
+        <v-col cols="12" sm="4" md="4" class="mt-2">
+          <CurriculumCard
+            :title="{
+              message: 'Personal Profile',
+              color: '#FF5722',
+            }"
+            :subHeader="['About me']"
+            :message="[
+              'I am a Computer Science Engineer with robust problem solving skills and proven experience in creating and designing software.',
+            ]"
+          />
+          <CurriculumCard
+            :title="{
+              message: 'Education',
+              color: '#FF5722',
+            }"
+            :subHeader="['Universidad Autónoma de Ciudad Juárez']"
+            :message="['Bachelors degree in Computer Science (2016-2021).']"
+          />
+          <CurriculumCard
+            :title="{
+              message: 'Professional summary',
+              color: '#FF5722',
+            }"
+            :subHeader="[
+              'Software developer in United DevOps (2020).',
+              'Software developer in Stoneridge (2021-present).',
+            ]"
+            :message="[
+              'Landing web page for a health care company and administration suite for itself maintainance.',
+              'Full stack developer of web software based on Lean Manufacturing Operational Systems, Visual Management and Business Intelligence.',
+            ]"
+          />
+        </v-col>
+        <v-col cols="12" sm="4" md="4" class="mt-2">
+          <CurriculumCard
+            :title="{
+              message: 'Skills',
+              color: '#FF5722',
+            }"
+            :bullets="
+            [
+              {
+                title: 'Technical skills',
+                message: [
+                  'C#',
+                  'Python',
+                  'TypeScript',
+                  'Vuetify',
+                  'VueJS',
+                  'Git',
+                  'Azure Data Studio',
+                  'SMSS',
+                  'Microsoft SQL Server',
+                  'Design Patterns',
+                  'SCRUM',
+                ]
+              },
+              {
+                title: 'Soft skills',
+                message: [
+                  'Teamwork',
+                  'Communication',
+                  'Problem solving',
+                  'Adaptability',
+                  'Time management',
+                  'Organization',
+                  'Creativity',
+                  'Initiative',
+                  'Self-learning',
+                  'Decision making',
+                ]
+              }
+            ]
+            "
+          />
+          <CurriculumCard
+            :title="{
+              message: 'Lenguages',
+              color: '#FF5722',
+            }"
+            :subHeader="['Spanish', 'English']"
+            :message="['Native speaker.', 'Advanced writing, reading and speaking. TOEFL ITP Test Score: 545 points.']"
+          />
         </v-col>
       </v-row>
     </center>
