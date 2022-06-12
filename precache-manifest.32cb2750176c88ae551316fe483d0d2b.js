fa904ea736898342edfd175081c76d219f3e168d
Juan Reynosa <al157706@alumnos.uacj.mx>
2022-06-12 17:45:50 -0600

    Added public path

---
+++ precache-manifest.32cb2750176c88ae551316fe483d0d2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7356d58de0c3bb04e0cb",
+    "url": "/curriculum-vitae/css/chunk-vendors.d19e80b0.css"
+  },
+  {
+    "revision": "cd8f6c33114c6e0e9ba9e0481670eb74",
+    "url": "/curriculum-vitae/img/PersonalPicture.cd8f6c33.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/curriculum-vitae/img/logo.png"
+  },
+  {
+    "revision": "d6c428bddbc1aa1c6937299f3a02ed89",
+    "url": "/curriculum-vitae/index.html"
+  },
+  {
+    "revision": "ab325e1d9ebbb798efc9",
+    "url": "/curriculum-vitae/js/app.00ecdc8e.js"
+  },
+  {
+    "revision": "7356d58de0c3bb04e0cb",
+    "url": "/curriculum-vitae/js/chunk-vendors.7a620836.js"
+  },
+  {
+    "revision": "e24a6ebd14c4fc6b6b9121a431d219ab",
+    "url": "/curriculum-vitae/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/curriculum-vitae/robots.txt"
+  }
+]);
